b00f3ccd3123f4c14b6f685a62baafaba76bacbf
Filippo43 <gabriel_filippo@rocketmail.com>
2016-06-10 12:34:42 -0300

    8th commit

--- JogoTiro.c
+++ JogoTiro.c
@@ -30,15 +30,6 @@
 #define WAVE5  20
 
 SDL_Texture* backgroundTexture = NULL;
-SDL_Texture* borderTexture = NULL;
-SDL_Texture* keyTexture = NULL;
-SDL_Rect keyRect;
-
-SDL_Texture* logoTexutre = NULL;
-SDL_Texture* fadeTexture = NULL;
-Uint8 alpha = 255;
-int fadeInDone = 0;
-int fadeOutDone = 0;
 
 void CreateComponents();
 void CreateFont();
@@ -53,6 +44,7 @@ void UpdateKeyBoard();
 void UpdateShotPosition();
 void PlayerVunerability();
 void CreateEnemy();
+void CreateBoss();
 void RemoveAt_Shot(ARRAYSHOT *arrayShot, int pos);
 void RemoveAt_Enemy(ARRAYENEMY *arrayEnemy, int pos);
 void CreateRound();
@@ -63,14 +55,15 @@ VECTOR2 GetMousePos();
 void AnimPlayer();
 void AnimateEnemy();
 
-void PlayerHandleEvent(SDL_Event e);
-
 
 int quit = 0;
 SDL_Event e;
 int gameDelay = 8;
 int score = 0;
 int wavesurvived = 0;
+int totalwaves = 0;
+int isboss = 0;
+int bossdamage = 0;
 
 WINDOW window;
 
@@ -90,53 +83,23 @@ SDL_Rect fontRect;
 //Loops Variables
 int i, j;
 
-enum SCENE{ INTRO, MENU, GAME };
-enum SCENE scene;
-
-//returns 1 if done
-int FadeIn(Uint8* alpha)
-{
-    if((*alpha) <= 0)
-        return 1;
-    else{
-        (*alpha) -= 1;
-    }
-
-    return 0;
-}
-
-int FadeOut(Uint8* alpha)
-{
-    if((*alpha) >= 254)
-        return 1;
-    else{
-        (*alpha) += 2;
-    }
-
-    return 0;
-}
 
 int main(void)
 {
+
     CreateWindow(&window, GAME_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
 	CreateComponents();
     CreateFont();
 
-    scene = INTRO;
 
-    keyRect.x = 650;
-    keyRect.y = 100;
-    keyRect.w = 34;
-    keyRect.h = 18; 
 
     //int w, h;
     //SDL_GetWindowSize(window, &w, &h);
 
-    backgroundTexture = loadTexture("Sprites/Background0.png");
-    borderTexture = loadTexture("Sprites/Border_Black.png");
-    fadeTexture = loadTexture("Sprites/Fade.png");
-    logoTexutre = loadTexture("Sprites/Logo.jpg");
-    keyTexture = loadTexture("Sprites/Key.png");
+    backgroundTexture = loadTexture("Sprites/BackgroundMenu.png");
+
     
 
 	while(quit == 0)
@@ -150,9 +113,24 @@ int main(void)
 
             WindowHandleEvent(e, &window);
 
-            if(scene == GAME || scene == MENU)
-            PlayerHandleEvent(e);
-           
+            VECTOR2 mousePos = GetMousePos();
+            LookAtMouse(&player, mousePos);
+
+            if(e.type == SDL_MOUSEBUTTONDOWN){
+
+                VECTOR2 playerPos;
+                playerPos.x = player.rect.x;
+                playerPos.y = player.rect.y;
+   
+
+                player.canShoot = 0;
+                VECTOR2 dir = GetVector(playerPos, mousePos);
+                dir = Normalize(dir);
+                Shoot(dir.x, dir.y);
+            }
+            if(e.type == SDL_MOUSEBUTTONUP && player.canShoot == 1 ){
+                player.canShoot = 1;
+            }
         }
 
         SDL_Delay(gameDelay);
@@ -163,87 +141,19 @@ int main(void)
         player.lifeRectBG.y = player.rect.y - 10;
         player.lifeRect.w = player.hp;
 
+        if (player.hp < 15 && player.hp > 6)
+        {
+        	player.lifeTexture = loadTexture("Sprites/Life2.png");
+        }
+
+        else if (player.hp < 7)
+        {
+        	player.lifeTexture = loadTexture("Sprites/Life3.png");
+        }
 
         Update();
         Draw();       
 	}
-}
-
-void PlayerHandleEvent(SDL_Event e)
-{
-     VECTOR2 mousePos = GetMousePos();
-    LookAtMouse(&player, mousePos);
-
-    if(e.type == SDL_MOUSEBUTTONDOWN){
-
-        VECTOR2 playerPos;
-        playerPos.x = player.rect.x;
-        playerPos.y = player.rect.y;
-
-
-        player.canShoot = 0;
-        VECTOR2 dir = GetVector(playerPos, mousePos);
-        dir = Normalize(dir);
-        Shoot(dir.x, dir.y);
-    }
-    if(e.type == SDL_MOUSEBUTTONUP && player.canShoot == 1 ){
-        player.canShoot = 1;
-    }
-}
-
-void DrawIntro()
-{
-    SDL_RenderCopy( window.renderer, logoTexutre, NULL, NULL );
-
-    SDL_SetTextureBlendMode(fadeTexture, SDL_BLENDMODE_BLEND);
-    SDL_SetTextureAlphaMod( fadeTexture, alpha );
-    SDL_RenderCopy( window.renderer, fadeTexture, NULL, NULL );
-    
-    if(fadeInDone == 0)
-        fadeInDone = FadeIn(&alpha);
-    else
-        fadeOutDone = FadeOut(&alpha);
-        
-    if(fadeOutDone == 1){
-        scene = MENU;
-        fadeInDone = 0;
-        fadeOutDone = 0;
-    }
-}
-
-void DrawMenu()
-{
-    SDL_RenderCopy( window.renderer, backgroundTexture, NULL, NULL );
-    
-    SDL_RenderCopy( window.renderer, keyTexture, NULL, &keyRect );
-    
-    if(fadeInDone == 0)
-        fadeInDone = FadeIn(&alpha);
-}
-
-
-
-void DrawGame()
-{
-        //DrawBackground
-        //SDL_RenderCopy( window.renderer, backgroundTexture, NULL, NULL );
-        
-        //Draw Player 
-        DrawPlayer(player, &window);
-         //Draw Shot
-        for( i=0; i<arrayShot.length; i++)
-             SDL_RenderCopy( window.renderer, arrayShot.defaultShotTexture, NULL, &arrayShot.vetor[i].rect );
-        //Draw Enemies
-        for( j=0; j<arrayEnemy.length; j++)
-             SDL_RenderCopyEx( window.renderer, arrayEnemy.vetor[j].texture, &arrayEnemy.vetor[j].imageRect, &arrayEnemy.vetor[j].rect, 0, NULL, SDL_FLIP_NONE);
-        //Draw Border
-        //SDL_RenderCopy( window.renderer, borderTexture, NULL, NULL );
-        //Draw Font
-        SDL_Color textColor = { 0xFF, 0xFF, 0xFF };
-        char scoreText[100];
-        sprintf(scoreText, "%i", score);
-        loadFromRenderedText(scoreText, textColor);
-        SDL_RenderCopy( window.renderer, scoreTexture, NULL, &fontRect);
 }
 
 void Draw()
@@ -254,31 +164,36 @@ void Draw()
     //Screen
     SDL_SetRenderDrawColor( window.renderer, 0000, 0000, 0000, 0000 );
     SDL_RenderClear(window.renderer);
-
-    if(scene == INTRO){
-        DrawIntro();
-    }
-    else if(scene == GAME || scene == MENU){
-        DrawMenu();
-        DrawGame();
-    }
+    //DrawBackground
+    SDL_RenderCopy( window.renderer, backgroundTexture, NULL, NULL );
+    //Draw Player 
+    DrawPlayer(player, &window);
+     //Draw Shot
+    for( i=0; i<arrayShot.length; i++)
+         SDL_RenderCopy( window.renderer, arrayShot.defaultShotTexture, NULL, &arrayShot.vetor[i].rect );
+    //Draw Enemies
+    for( j=0; j<arrayEnemy.length; j++)
+         SDL_RenderCopyEx( window.renderer, arrayEnemy.vetor[j].texture, &arrayEnemy.vetor[j].imageRect, &arrayEnemy.vetor[j].rect, 0, NULL, SDL_FLIP_NONE);
+    //Draw Font
+    SDL_Color textColor = { 0xFF, 0xFF, 0xFF };
+    char scoreText[100];
+    sprintf(scoreText, "%i", score);
+    loadFromRenderedText(scoreText, textColor);
+    SDL_RenderCopy( window.renderer, scoreTexture, NULL, &fontRect);
+   
 
     SDL_RenderPresent(window.renderer); 
 }
 
 void Update()
 {
-    if(scene == GAME || scene == MENU){
-        UpdateShotPosition();
-        UpdateKeyBoard();
-        Collision();
-    }
-    else if(scene == GAME){
-        PlayerVunerability();
-        GameOver();
-        CreateRound();
-        AnimateEnemy();
-    }
+    UpdateKeyBoard();
+    UpdateShotPosition();
+    PlayerVunerability();
+    GameOver();
+    CreateRound();
+    Collision();
+    AnimateEnemy();
 }
 
 void Collision()
@@ -288,10 +203,27 @@ void Collision()
     //Collision Shot and Enemy
     for(j = 0; j<arrayEnemy.length; j++){
         for( i = 0; i<arrayShot.length; i++){
+
             if(Collision_Rect_Rect(arrayEnemy.vetor[j].rect, arrayShot.vetor[i].rect) == 1){
+                
+                if(isboss == 1)
+                {
+                    RemoveAt_Shot(&arrayShot, i);
+                    bossdamage ++;
+
+                    if(bossdamage == 10)
+                    {
+                        RemoveAt_Enemy(&arrayEnemy, j);
+                        isboss = 0;
+                        score += 10;
+                        bossdamage = 0;
+                    }
+                }
+                else if (isboss == 0){
                 RemoveAt_Enemy(&arrayEnemy, j);
                 RemoveAt_Shot(&arrayShot, i);
                 score++;
+                }
             }
         }
     }
@@ -299,14 +231,33 @@ void Collision()
      //Collison Enemy_border + follow player
     for(j = 0; j< arrayEnemy.length; j++){
        // arrayEnemy.vetor[j].rect = Collision_Rect_Screen(arrayEnemy.vetor[j].rect, SCREEN_WIDTH, SCREEN_HEIGHT);
+
         arrayEnemy.vetor[j].rect = ChasePlayer(arrayEnemy.vetor[j].rect, player.rect, &arrayEnemy.vetor[j]);
     }
 
      //Collision Enemy_Player
     for(j = 0; j<arrayEnemy.length; j++){        
        if(Collision_Rect_Rect(player.rect, arrayEnemy.vetor[j].rect) == 1 && player.vulnerable == 1){
-            player.hp--;
+            if (isboss == 1){
+            player.hp -= 10;
             player.vulnerable = 0;
+
+            }
+
+            else if (isboss == 0)
+            {
+                if (wavesurvived >= 3)
+                {
+                player.hp -= 2;
+                player.vulnerable = 0;
+                }
+                else
+                {
+                   player.hp --;
+                   player.vulnerable = 0; 
+                }
+
+            }
        }
     }
 
@@ -380,76 +331,71 @@ void RemoveAt_Enemy(ARRAYENEMY *arrayEnemy, int pos)
 
 
 int posVector[24];
-void CreateRound()
-{
+void CreateRound(){
 
 
 
     for(i = 0; i< 24 ; i++)
         posVector[i] = 0;
 
-    if (wavesurvived == 6)
-        wavesurvived = 0;
+    if (wavesurvived >= 5)
+    {
+    	totalwaves += wavesurvived;
+    	wavesurvived -= 5;
+    }
 
     if(arrayEnemy.length == 0){
+       
         if (wavesurvived == 0){
 
-            for(i=0; i<WAVE1; i++){
+        	for(i=0; i<WAVE1; i++){
             CreateEnemy();
-            
-            
-            }
-             wavesurvived ++;
+           	}
+            wavesurvived ++;
         }
         else if (wavesurvived == 1){
 
-            for(i=0; i<WAVE2; i++){
+        	for(i=0; i<WAVE2; i++){
             CreateEnemy();
-           
             }
-             wavesurvived ++;
+            wavesurvived ++;
         }
         else if (wavesurvived == 2){
 
-            for(i=0; i<WAVE3; i++){
-            CreateEnemy();
+        	CreateBoss();
+
             
-            
-            }
-             wavesurvived ++;
-        }
-         
+            wavesurvived ++;
+        
+         }
          else if (wavesurvived == 3){
 
-            for(i=0; i<WAVE4; i++){
+        	for(i=0; i<WAVE4; i++){
             CreateEnemy();
-            
             }
-             wavesurvived ++;
+            wavesurvived ++;
          }
          else if (wavesurvived == 4){
 
-
-
-            for(i=0; i<WAVE5; i++){
+        	for(i=0; i<WAVE5; i++){
             CreateEnemy();
-            
-            
             }
-
-             wavesurvived ++;
-             
+            wavesurvived ++;
+        
          }
 
     }
 
-   
+    
     
     
 }
-
 void GameOver(){
     if(player.hp <= 0){
+        
+        score = 0;
+        wavesurvived = 0;
+        
         CreateComponents();
     }
 }
@@ -457,8 +403,8 @@ void GameOver(){
 
 void CreateEnemy()
 {
-    int x[4] = {SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2, SCREEN_WIDTH};
-    int y[4] = {0, SCREEN_HEIGHT/2, SCREEN_HEIGHT, SCREEN_HEIGHT/2};
+    int x[4] = {365, -30, 365, SCREEN_WIDTH + 30};
+    int y[4] = {-30, 250, SCREEN_HEIGHT + 30, 250};
     int quad;
     int pos;
     int sortedNumber;
@@ -473,11 +419,11 @@ void CreateEnemy()
 
         if(quad % 2 == 1){
         x[quad] -= pos%3 * 41;
-        y[quad] += pos/3 * 39;
+        y[quad] += pos/3 * 20;
     }
     else {
         y[quad] -= pos%3 * 41;
-        x[quad] += pos/3 * 39;
+        x[quad] += pos/3 * 20;
     }
         
 
@@ -487,7 +433,7 @@ void CreateEnemy()
     ENEMY enemy;
     enemy.rect.x = x[quad];
     enemy.rect.y = y[quad];
-    enemy.rect.w = 32;
+    enemy.rect.w = 41;
     enemy.rect.h = 32;
     enemy.speedH = 1;
     enemy.speedV = 1;
@@ -500,16 +446,81 @@ void CreateEnemy()
     enemy.imageRect.h = 16;
     enemy.spriteMax = 2;
     enemy.delaySprite = 0;
+    
 
-    if (wavesurvived == 5)
+    if (totalwaves >= 5)
     {
         enemy.speedH = 2;
         enemy.speedV = 2;
-        wavesurvived ++;
     }
+ 
+    if (wavesurvived >= 3)
+    {
+        enemy.texture = loadTexture("Sprites/skeleton.png");  
+        enemy.spriteMax = 3;
+        enemy.spritePos = rand() % 3;
+    }
+
+  
 
     arrayEnemy.vetor[arrayEnemy.length] = enemy;
     arrayEnemy.length++;
+    
+}
+
+void CreateBoss()
+{
+    int x[4] = {365, -30, 365, SCREEN_WIDTH + 30};
+    int y[4] = {-30, 250, SCREEN_HEIGHT + 30, 250};
+    int quad;
+    int pos;
+    int sortedNumber;
+
+     do{
+            sortedNumber = rand() % 24;
+            pos = sortedNumber % 6;
+            quad = sortedNumber / 6;
+    }while(posVector[sortedNumber]);
+
+        posVector[sortedNumber] = 1;
+
+        if(quad % 2 == 1){
+        x[quad] -= pos%3 * 41;
+        y[quad] += pos/3 * 20;
+    }
+    else {
+        y[quad] -= pos%3 * 41;
+        x[quad] += pos/3 * 20;
+    }
+        
+
+
+    
+    
+    ENEMY enemy;
+    enemy.rect.x = x[quad];
+    enemy.rect.y = y[quad];
+    enemy.rect.w = 41;
+    enemy.rect.h = 32;
+    enemy.speedH = 2;
+    enemy.speedV = 2;
+    enemy.texture = loadTexture("Sprites/bat.png");
+    enemy.countIA = 0;
+    enemy.imageRect.x = 0;
+    enemy.imageRect.y = 0;
+    enemy.spritePos = rand() % 3;
+    enemy.imageRect.w =  16;
+    enemy.imageRect.h = 16;
+    enemy.spriteMax = 2;
+    enemy.delaySprite = 0;
+
+    isboss = 1;
+    
+  
+
+    arrayEnemy.vetor[arrayEnemy.length] = enemy;
+    arrayEnemy.length++;
+
     
 }
 
@@ -621,36 +632,36 @@ void UpdateKeyBoard()
     arrayShot.delay++;
     if(keystates[ SDL_SCANCODE_UP] && keystates[ SDL_SCANCODE_RIGHT] && arrayShot.delay >= arrayShot.delayMin)
     {
-        Shoot(1, -1);    
+        Shoot(2, -2);    
     }
     else if(keystates[ SDL_SCANCODE_UP] && keystates[ SDL_SCANCODE_LEFT] && arrayShot.delay >= arrayShot.delayMin)
     {
-        Shoot(-1, -1);    
+        Shoot(-2, -2);    
     }
     else if(keystates[ SDL_SCANCODE_DOWN] && keystates[ SDL_SCANCODE_LEFT] && arrayShot.delay >= arrayShot.delayMin)
     {        
-        Shoot(-1, 1);    
+        Shoot(-2, 2);    
     }
     else if(keystates[ SDL_SCANCODE_DOWN] && keystates[ SDL_SCANCODE_RIGHT] && arrayShot.delay >= arrayShot.delayMin)
     {   
-        Shoot(1, 1);    
+        Shoot(2, 2);    
     }
 
     else if(keystates[ SDL_SCANCODE_UP] && arrayShot.delay >= arrayShot.delayMin)
     {       
-        Shoot(0, -1);     
+        Shoot(0, -2);     
     }  
     else if(keystates[ SDL_SCANCODE_DOWN] && arrayShot.delay >= arrayShot.delayMin)
     {
-        Shoot(0, 1 );     
+        Shoot(0, 2 );     
     } 
     else if(keystates[ SDL_SCANCODE_RIGHT] && arrayShot.delay >= arrayShot.delayMin)
     {
-        Shoot(1, 0 );     
+        Shoot(2, 0 );     
     }  
     else if(keystates[ SDL_SCANCODE_LEFT] && arrayShot.delay >= arrayShot.delayMin)
     {
-        Shoot(-1, 0 );     
+        Shoot(-2, 0 );     
     }  
 
 }
@@ -669,6 +680,8 @@ void AnimPlayer(PLAYER* player)
 }
 
 void CreateComponents(){
+
+
     player.rect.x = SCREEN_WIDTH / 2 - 47/2;
     player.rect.y = SCREEN_HEIGHT / 2 - 22/2;
     player.rect.w = 47;
