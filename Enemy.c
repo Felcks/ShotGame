77e82148b6f129cf02a538bcb09b9321fb23c52e
Filippo43 <gabriel_filippo@rocketmail.com>
2016-06-10 13:01:40 -0300

    teste

--- Enemy.c
+++ Enemy.c
@@ -8,47 +8,15 @@ SDL_Rect ChasePlayer(SDL_Rect enemyRect, SDL_Rect playerRect, ENEMY* enemy)
 
 	(*enemy).countIA = 0;
 
-	
-	if (enemyRect.x + (*enemy).rect.w > 110 && enemyRect.x + (*enemy).rect.w < 690 && enemyRect.y + (*enemy).rect.h > 100 && enemyRect.y + (*enemy).rect.h < 510 ){
-
-		if(enemyRect.x < playerRect.x)
-			enemyRect.x += (*enemy).speedH;
-
-		else if (enemyRect.x > playerRect.x)
-			enemyRect.x -= (*enemy).speedH;
-
-		if(enemyRect.y < playerRect.y)
-			enemyRect.y += (*enemy).speedV;
-		else
-			enemyRect.y -= (*enemy).speedV;
-
-		return enemyRect;
-
-	}
+	if(enemyRect.x < playerRect.x)
+		enemyRect.x += (*enemy).speedH;
 	else
-	{
-		if (enemyRect.x + (*enemy).rect.w <= 110)
-		{
-				enemyRect.x += (*enemy).speedH;
-		}
+		enemyRect.x -= (*enemy).speedH;
 
-		if (enemyRect.x + (*enemy).rect.w >= 690)
-		{
-				enemyRect.x -= (*enemy).speedH;
-		}
+	if(enemyRect.y < playerRect.y)
+		enemyRect.y += (*enemy).speedV;
+	else
+		enemyRect.y -= (*enemy).speedV;
 
-		if (enemyRect.y + (*enemy).rect.h <= 100 )
-		{
-				enemyRect.y += (*enemy).speedV;
-		}
-
-		if (enemyRect.y + (*enemy).rect.h >= 510 )
-		{
-				enemyRect.y -= (*enemy).speedV;
-		}
-
-		
-		return enemyRect;
-	}
-	
+	return enemyRect;
 }
